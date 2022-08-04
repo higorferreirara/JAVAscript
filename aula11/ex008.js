1d92d5c80dd60b3677131bd45c3967bb3dc94f33
Higor Ferreira <108104952+higorferreirara@users.noreply.github.com>
2022-08-03 22:11:07 -0300

    condições

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60.5
+console.log(`A velocidade do meu carro é ${vel}km/h`)
+if (vel > 60){
+    console.log('Você passou a velocidade permitida, MULTADO!')
+}
+console.log('Dirija sempre usando o sinto de segurança!')
