cf144430b88db83a3b65219b6d6b3a5e6bf720b4
Higor Ferreira <108104952+higorferreirara@users.noreply.github.com>
2022-08-10 22:58:35 -0300

    correção

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -2,8 +2,7 @@ function carregar() {
     var msg = document.getElementById('msg')
     var img = document.getElementById('imagem')
     var data = new Date()
-    //var hora = data.getHours()
-    var hora = 2
+    var hora = data.getHours()
     msg.innerHTML = `Agora são ${hora}:00 horas.`
 
     if (hora >= 0 && hora < 12){
