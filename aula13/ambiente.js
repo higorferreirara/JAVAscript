2b7fb30f039349ddd3966bf873a73de8dd01efb4
Higor Ferreira <108104952+higorferreirara@users.noreply.github.com>
2022-08-12 19:57:00 -0300

    repetiçoes

---
+++ aula13/ambiente.js
@@ -0,0 +1,14 @@
+/*var tudo = 8
+while (tudo > 0){
+console.log('Tudo bem?')
+tudo -= 1
+}
+console.log('Fim')
+*/
+
+var c = 0
+do{
+    console.log('Te amo!')
+    c++
+}while(c<=8)
+console.log('FIM!')
